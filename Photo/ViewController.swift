681b977e52484a731ec153988f0892ad8f2479cc
Dmitry Pavlov <perr07@yandex.ru>
2022-02-02 00:32:48 +0300

    Initial Commit

---
+++ Photo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Photo
+//
+//  Created by Pandos on 02.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
